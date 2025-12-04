404c97d7f3dd430f62275815919fdb2699f9e0c0
SREEDEEP DEY <152058891+SREEDEEP-DEY@users.noreply.github.com>
2025-12-04 11:55:20 +0530

    Update db.py

--- db.py
+++ db.py
@@ -4,6 +4,7 @@ import json
 import psycopg2
 from psycopg2.extras import execute_values
 
+DATABASE_URL=os.getenv("DATABASE_URL")
 DB_HOST = os.getenv("DB_HOST", "localhost")
 DB_PORT = int(os.getenv("DB_PORT", "5432"))
 DB_NAME = os.getenv("DB_NAME", "finparse")
@@ -12,6 +13,8 @@ DB_PASSWORD = os.getenv("DB_PASSWORD", "finpass")
 
 
 def get_db_connection():
+    if DATABASE_URL:
+        return psycopg2.connect(DATABASE_URL, sslmode="require")
     return psycopg2.connect(
         host=DB_HOST,
         port=DB_PORT,
